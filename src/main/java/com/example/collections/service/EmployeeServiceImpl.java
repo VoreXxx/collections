a40156e43536f945cc962b0478b4449e7e495914
VoreXxx <vorex.00@mail.ru>
2024-01-21 16:58:04 +0600

    Homework done

--- src/main/java/com/example/collections/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/collections/service/EmployeeServiceImpl.java
@@ -3,9 +3,15 @@ package com.example.collections.service;
 import com.example.collections.Employee;
 import com.example.collections.exception.EmployeeAlreadyAddedException;
 import com.example.collections.exception.EmployeeNotFoundException;
+import com.example.collections.exception.InvalidNameException;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+
+import static org.apache.commons.lang3.StringUtils.capitalize;
+import static org.apache.commons.lang3.StringUtils.isAlpha;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -28,7 +34,13 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee add(String firstName, String lastName, int salary, int departmentId) {
-        Employee employee = new Employee(firstName, lastName, salary, departmentId);
+        validateNames(firstName, lastName);
+        Employee employee = new Employee(
+                capitalize(firstName),
+                capitalize(lastName),
+                salary,
+                departmentId
+        );
         if (employees.containsKey(employee.getFullName())) {
             throw new EmployeeAlreadyAddedException();
         }
@@ -38,6 +50,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee remove(String firstName, String lastName, int salary, int departmentId) {
+        validateNames(firstName, lastName);
         Employee employee = new Employee(firstName, lastName, salary, departmentId);
         if (employees.containsKey(employee.getFullName())) {
             return employees.remove(employee.getFullName());
@@ -46,12 +59,13 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
-    public Employee find(String firstname, String lastname) {
+    public Employee find(String firstName, String lastName) {
         return null;
     }
 
     @Override
     public Employee find(String firstName, String lastName, int salary, int departmentId) {
+        validateNames(firstName, lastName);
         Employee employee = new Employee(firstName, lastName, salary, departmentId);
         if (employees.containsKey(employee.getFullName())) {
             return employees.get(employee.getFullName());
@@ -62,5 +76,18 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public Collection<Employee> findAll() {
         return Collections.unmodifiableCollection(employees.values());
+    }
+
+    private void validateNames(@NotNull String... names) {
+        for (String name : names) {
+            if (!isAlpha(name)) {
+                throw new InvalidNameException(name);
+            }
+        }
+    }
+
+    @Contract(pure = true)
+    private @NotNull String getKey(String firstName, String lastName) {
+        return (firstName + " " + lastName).toLowerCase();
     }
 }
